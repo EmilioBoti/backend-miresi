ebcc9de8910315df3b34cf079b8732254f924c5a
emilio <emiliobotier@gmail.com>
2022-02-01 22:48:09 +0100

    firts deploy in heroku

---
+++ public/main.js
@@ -0,0 +1,3 @@
+//var io = io()
+//var socket = io.connect("http://192.168.33.94:3000", { forceNew: true });
+
---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const socket = require("socket.io")
+const app = express()
+// var server = require("http").Server(app);
+
+const port = 3000
+const hostname = "192.168.33.28"
+
+const server = app.listen(port, hostname,()=>{
+    console.log(`listenning on port: ${port}`)
+})
+
+const json = {
+    "id": 5,
+    "name": "Emilio"
+}
+
+app.use(express.static("public"));
+const io = socket(server)
+
+
+app.get("/", (req, res)=>{
+    res.status(200).send("<h1>Hello World</h1>")
+})
+
+io.on("connection", (socket)=>{
+    console.log(`User has connected: ${socket.id}`)
+
+    socket.on("message", (data)=>{
+        console.log(`data from ${socket.id}: ${data}`)
+        io.emit("newMessage", data)
+    })
+})
+
