45211b6c44e4ee254dfe678250f7b7529122e14e
emilio <emiliobotier@gmail.com>
2022-02-07 12:26:08 +0100

    backend

--- server/index.js
+++ server/index.js
@@ -1,38 +1,47 @@
 const express = require("express");
 const socket = require("socket.io")
 const app = express()
-// var server = require("http").Server(app);
 
 const port = 3000
-const hostname = "192.168.1.129"
-const hostname2 = "https://miresi-chat.herokuapp.com/"
+const hostname = "192.168.33.99"
 
 //settings 
 app.set('port', process.env.PORT || 3000)
 
-const server = app.listen(app.get('port'),()=>{
+const hostname2 = app.get('port')
+
+const server = app.listen(port, hostname,()=>{
     console.log(`listenning on port: ${port}`)
 })
 
-const json = {
-    "id": 5,
-    "name": "Emilio"
-}
-
-app.use(express.static("public"));
+//app.use(express.static("public"));
 const io = socket(server)
+const dataUser = [] 
 
-
-app.get("/", (req, res)=>{
-    res.status(200).send("<h1>Hello World</h1>")
-})
 
 io.on("connection", (socket)=>{
     console.log(`User has connected: ${socket.id}`)
-
-    socket.on("message", (data)=>{
-        console.log(`data from ${socket.id}: ${data}`)
-        io.emit("newMessage", data)
+    socket.on("user", (data)=>{
+        const obj = JSON.parse(data)
+        let exists = dataUser.some((e) => e.id == socket.id)
+        if(!exists) dataUser.push(obj)      
+        console.log(`${exists}`)
+        io.emit("userConnected", dataUser)
     })
-})
+    
+    socket.on("boxMessage", (socketId)=>{
+        //socket.join(socketId)
+        //console.log(socket.rooms)
+        socket.on("message", (data, userId)=>{
+            let us =  dataUser.find((e)=>{
+                if(e.id === userId){
+                    return e.id
+                }
+            })   
+            socket.to(socketId).emit("private", data)
+            console.log(us)
+        })
+    })
+
+})  
 
