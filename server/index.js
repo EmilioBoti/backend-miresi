9c67037bc7487fdbc675a72c96c0be087a9ecfaa
emilio <emiliobotier@gmail.com>
2022-02-08 01:09:34 +0100

    improvement

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const socket = require("socket.io")
 const app = express()
 
 const port = 3000
-const hostname = "192.168.33.99"
+const hostname = "192.168.1.129"
 
 //settings 
 app.set('port', process.env.PORT || 3000)
@@ -21,26 +21,22 @@ const dataUser = []
 
 io.on("connection", (socket)=>{
     console.log(`User has connected: ${socket.id}`)
+    
     socket.on("user", (data)=>{
         const obj = JSON.parse(data)
         let exists = dataUser.some((e) => e.id == socket.id)
-        if(!exists) dataUser.push(obj)      
+        if(!exists) dataUser.push(obj)
         console.log(`${exists}`)
         io.emit("userConnected", dataUser)
     })
     
-    socket.on("boxMessage", (socketId)=>{
-        //socket.join(socketId)
-        //console.log(socket.rooms)
-        socket.on("message", (data, userId)=>{
-            let us =  dataUser.find((e)=>{
-                if(e.id === userId){
-                    return e.id
-                }
-            })   
-            socket.to(socketId).emit("private", data)
-            console.log(us)
-        })
+    // socket.on("boxMessage", (socketIdSender)=>{ })
+
+    socket.on("message", (data, userId)=>{
+        let byWhose = JSON.parse(data)
+        let us =  dataUser.find((e)=> e.id === userId)
+        socket.to(userId).emit("private", byWhose)
+        console.log(us)
     })
 
 })  
