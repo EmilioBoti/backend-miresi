9f1bc94724649ad9997f8a2963d4499fd434c481
emilio <emiliobotier@gmail.com>
2022-02-10 11:48:02 +0100

    improved chat

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -2,10 +2,11 @@
 const mysql = require("mysql")
 
 const connectionDb = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "miresidb"
+    host: "sql11.freemysqlhosting.net",
+    port: 3306,
+    user: "sql11471646",
+    password: "WirNQflQBL",
+    database: "sql11471646",
 })
 
 module.exports = connectionDb
--- server/index.js
+++ server/index.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const socket = require("socket.io")
+const encrypt = require("./encrypting")
+
 const app = express()
 
 const mysql = require("./dbConnect")
 
 const port = 3000
-const hostname = "192.168.1.129"
+const hostname = "192.168.32.244"
 const hostname2 = app.get('port')
+
+const objEncrypt =  encrypt.encrypting("holaaaa")
 
 //settings 
 app.set('port', process.env.PORT || 3000)
 
 const server = app.listen(port, hostname,()=>{
     console.log(`listenning on port: ${port}`)
+    console.log(objEncrypt.content)
+    console.log(encrypt.decrypt(objEncrypt))
 })
 
 //connect to DDBB MYSQL
-// mysql.connect((error)=>{
-//     if(error) throw error
-//     else console.log("Connected")
-// })
+mysql.connect((error)=>{
+    if(error) throw error
+    else console.log("Connected")
+})
 
 app.get("/", (req, res)=>{
+
+    mysql.query("Select * from register_users", (err, result)=>{
+        if(err) throw err
+        console.log(result)
+    })
     res.send("hola")
 })
 
@@ -29,32 +40,21 @@ app.get("/", (req, res)=>{
 const io = socket(server)
 const dataUser = [] 
 
-
 io.on("connection", (socket)=>{
     let idSocket = socket.id
-    console.log(`User has connected: ${idSocket}`)
+    // console.log(`User has connected: ${idSocket}`)
     
     socket.on("user", (data)=>{
         const obj = JSON.parse(data)
         // let exists = dataUser.some((e) => e.id == idSocket)
-        // if(!exists)
         dataUser.push(obj)
-        console.log(`${exists}`)
         io.emit("userConnected", dataUser)
     })
     
     //events happen when a message is send
     socket.on("message", (data,sendToUserId,senderId)=>{
         let byWhose = JSON.parse(data)
-        let us =  dataUser.find((e)=> e.id === sendToUserId)
-
-        socket.to(sendToUserId).to(senderId).emit("private", byWhose) //emit events to both socket sender and receiver
-
-        console.log(`Sender:`)
-        console.log(senderId)
-        console.log(`Receiver`)
-        console.log(us)
+        // let us =  dataUser.find((e)=> e.id === sendToUserId)
+        io.to(sendToUserId).to(senderId).emit("private", byWhose) //emit events to both socket sender and receiver
     })
-
 })  
-
