8cbef0c3b4bd997a9f0bd66f336494392c69d7a0
emilio <emiliobotier@gmail.com>
2022-02-11 01:48:33 +0100

    started API

--- server/index.js
+++ server/index.js
@@ -4,21 +4,26 @@ const encrypt = require("./encrypting")
 
 const app = express()
 
-const mysql = require("./dbConnect")
+const mysql = require("./dbConnect");
+const { JSON } = require("mysql/lib/protocol/constants/types");
+const { stringify } = require("nodemon/lib/utils");
+const { query } = require("express");
 
 const port = 3000
-const hostname = "192.168.32.244"
+const hostname = "192.168.1.129"
 
-const objEncrypt =  encrypt.encrypting("holaaaa")
+// const objEncrypt =  encrypt.encrypting("holaaaa")
 
 //settings 
 app.set('port', process.env.PORT || 3000)
 const hostname2 = app.get('port')
 
+
+//middlewares
+app.use(express.json())
+
 const server = app.listen(hostname2,()=>{
     console.log(`listenning on port: ${port}`)
-    console.log(objEncrypt.content)
-    console.log(encrypt.decrypt(objEncrypt))
 })
 
 //connect to DDBB MYSQL
@@ -27,13 +32,43 @@ mysql.connect((error)=>{
     else console.log("Connected")
 })
 
-app.get("/", (req, res)=>{
+//endpoints
+app.post("/api/v1/signup", (req, res)=>{
 
-    mysql.query("Select * from register_users", (err, result)=>{
+    const userToRegister = req.body
+    const encryptPW = encrypt.encrypting(userToRegister.password) 
+    userToRegister.password = encryptPW.content
+    const query = `INSERT INTO register_user (name, email, password) VALUES ('${userToRegister.name}','${userToRegister.email}', '${userToRegister.password}')`
+    
+    mysql.query(query, (err, result)=>{
         if(err) throw err
         console.log(result)
     })
-    res.send("hola")
+    return res.send(userToRegister)
+})
+
+app.post("/api/v1/chat", (req, res)=>{
+    const message = req.body
+    const query = `INSERT INTO chat (id_u_sender, id_u_receiver, message, checked)
+    VALUES ('${message.id_sender}','${message.id_receiver}', '${message.message}', '${message.chekced}')`
+    
+    mysql.query(query, (err, result)=>{
+        if(err) throw err
+        return res.send(message)
+    })
+})
+
+app.get("/api/v1/chat/:idSender/:idReceiver", (req, res)=>{
+    const { id, id2 } = req.params
+
+    const query = `SELECT * FROM chat WHERE
+    (id_u_sender = ${idSender} AND id_u_receiver = ${idReceiver})
+     or (id_u_sender = ${idReceiver} AND id_u_receiver = ${idSender}) `
+    
+    mysql.query(query, (err, result)=>{
+        if(err) throw err
+        return res.send(result)
+    })
 })
 
 //app.use(express.static("public"));
@@ -46,7 +81,6 @@ io.on("connection", (socket)=>{
     
     socket.on("user", (data)=>{
         const obj = JSON.parse(data)
-        // let exists = dataUser.some((e) => e.id == idSocket)
         dataUser.push(obj)
         io.emit("userConnected", dataUser)
     })
