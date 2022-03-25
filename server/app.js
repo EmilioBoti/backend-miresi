f9030c5a65a923c866773800e2a6706a641de78b
emilio <emiliobotier@gmail.com>
2022-03-25 03:23:34 +0100

    update router

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ require("dotenv").config()
 
 const express = require("express");
 const router = require("./router/router")
+const fs = require("fs")
+const mysql = require("./db/dbConnect")
+
 // const {cloudinaryConfig, uploader } = require("./config/cloudinaryConfig")
 
 const app = express()
 const port = 3000
 
- 
 //settings 
 app.set('port', process.env.PORT || port)
 
@@ -18,6 +20,49 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 // app.use("*", cloudinaryConfig)
 app.use('/api', router)
+
+// insetRoom('C:\\Users\\emili\\Desktop\\MiResiResearchRooms.csv')
+
+//'C:\\Users\\emili\\Desktop\\MiResiResearchTypeofRooms.csv'
+
+function insetRoom(path){
+    const allFileContents = fs.readFileSync(path, 'utf-8');
+    const lines = allFileContents.split(/\r?\n/)
+
+    for(let i = 5; i < 7; i++){
+        const lineArr = lines[i].split(",")
+
+        const query = `INSERT INTO room (name, id_type, price, AC, Calefaccion, Fridge)
+        VALUES ('${lineArr[3]}', 3, ${Number(lineArr[5].slice(1))}, ${lineArr[6]}, ${lineArr[7]}, ${lineArr[8]})`
+        
+        console.log(lineArr)
+        console.log(query)
+        // mysql.query(query, (err, result)=>{
+        //     if(err) throw err
+        //     console.log(result)
+        // })
+    }
+
+
+}
+
+//not part of the code
+function insertRoomType(){
+    const allFileContents = fs.readFileSync('C:\\Users\\emili\\Desktop\\MiResiResearchTypeofRooms.csv', 'utf-8');
+    const lines = allFileContents.split(/\r?\n/)
+    
+    for(let i = 1; i < lines.length-1; i++){
+        const lineArr = lines[i].split(",")
+        const query = `INSERT INTO room_type (type_name, kitchen, shared_bathroom, access_wheelchair, shared_kitchen, shared_room, type)
+        VALUES ('${lineArr[0]}',${lineArr[5]},${lineArr[6]},${lineArr[7]},${lineArr[8]},${lineArr[9]},'${lineArr[1]}')`
+    
+        mysql.query(query, (err, result)=>{
+            if(err) throw err
+            console.log(result)
+        })
+    }
+}
+
 
 
 module.exports = app
--- server/router/router.js
+++ server/router/router.js
@@ -1,25 +1,73 @@
 const app = require("express")
 const mysql = require("../db/dbConnect");
 const encrypt = require("../encrypting")
-// const path = require("path")
-// const multer = require("multer")
-// const { cloudinaryConfig, uploader } = require("../config/cloudinaryConfig")
 
 const router = app.Router()
 
 
-// const storage = multer.memoryStorage()
-// const multerUpload = multer({storage: storage}).single("img")
+//get a particular resi
+router.get('/v1/resi/:id', (req, res)=>{
+    try {
+        const { id } = req.params
+        const query = `SELECT residence.id, residence.name AS resiName, residence.location,residence.phone_number,
+        residence.description,residence.email, residence.link, residence.library, residence.laundry, residence.gym,
+        residence.parking_bicycle, residence.parking_car,residence.parking_motorcycle, residence.id_city,
+        picturesresidence.image
+        FROM residence
+        LEFT JOIN picturesresidence ON residence.id = picturesresidence.id_residence
+        WHERE residence.id = ${id}
+        GROUP BY residence.name`
 
-router.get('/v1/residences/:id', (req, res)=>{
+        mysql.query(query, (err, result)=>{
+            if(err) throw err
+            return res.json(result)
+        })
 
-    const { id } = req.params
-    const query = `SELECT residence.id, residence.name AS resiName, residence.location,residence.phone_number,
-    residence.description,residence.email, residence.link, residence.library, residence.laundry, residence.gym,
-    residence.parking_bicycle, residence.parking_car,residence.parking_motorcycle, residence.id_city
-    FROM residence INNER JOIN city ON residence.id_city = city.id WHERE city.name = '${id}'`
+    } catch (error) {
+        return res.json(null)
+    }
+
+})
+
+
+//get rooms from a particular resi
+router.get('/v1/resirooms/:idresi', (req, res)=>{
+    try {
+        const { idresi } = req.params
+        
+        const query = `SELECT * FROM room INNER JOIN room_type ON room.id_type = room_type.id
+        INNER JOIN picture_rooms ON picture_rooms.id_room_type = room_type.id
+        INNER JOIN residence ON residence.id = room.id_resi
+        WHERE residence.id = ${idresi} 
+        GROUP BY room.id        
+        `
+        mysql.query(query, (err, result)=>{
+            if(err) throw err
+            return res.json(result)
+        })
+
+    } catch (err) {
+        return res.json(null)
+    }
+
+})
+
+router.get('/v1/residences/:city', (req, res)=>{
 
     try {
+        const { city } = req.params
+        const query = `SELECT residence.id, residence.name AS resiName, residence.location,residence.phone_number,
+        residence.description,residence.email, residence.link, residence.library, residence.laundry, residence.gym,
+        residence.parking_bicycle, residence.parking_car,residence.parking_motorcycle, residence.id_city,
+        picturesresidence.image, stackfavourite.id_residence as idResiFavorite, stackfavourite.id_user as idUser,
+        MIN(room.price) as priceFrom 
+        FROM residence INNER JOIN city ON residence.id_city = city.id
+        LEFT JOIN stackfavourite ON stackfavourite.id_residence = residence.id
+        LEFT JOIN picturesresidence ON picturesresidence.id_residence = residence.id
+        LEFT JOIN room ON room.id_resi = residence.id
+        WHERE city.name = '${city}'
+        GROUP BY resiName`
+
         mysql.query(query, (err, result)=>{
             if(err) throw err
             return res.json(result)
@@ -31,11 +79,10 @@ router.get('/v1/residences/:id', (req, res)=>{
 })
 
 router.get('/city/c/:search', (req, res)=>{
-
-    const { search } = req.params
-    const query = `SELECT * FROM city WHERE name LIKE '${search}%'`
-
     try {
+        const { search } = req.params
+        const query = `SELECT * FROM city WHERE name LIKE '${search}%'`
+
         mysql.query(query, (err, result)=>{
             if(err) throw err
             return res.json(result)
@@ -45,22 +92,6 @@ router.get('/city/c/:search', (req, res)=>{
     }
 
 })
-
-// router.get('/city/c/:name', (req, res)=>{
-
-//     const { name } = req.params
-//     const query = `SELECT * FROM city WHERE name = '${name}'`
-
-//     try {
-//         mysql.query(query, (err, result)=>{
-//             if(err) throw err
-//             return res.json(result)
-//         })
-//     } catch (err) {
-//         return res.json(null)
-//     }
-
-// })
 
 router.get('/v1/city/:id', (req, res)=>{
 
@@ -130,6 +161,7 @@ router.post('/v1/login', (req, res)=>{
     }
 })
 
+
 //get all cities
 router.get("/v1/cities", (req, res)=>{    
     const query = `SELECT * FROM city`
@@ -153,11 +185,32 @@ router.get("/v1/chat/:idSender", (req, res)=>{
     })
 })
 
-router.get("/v1/chat/:idSender/:idReceiver", (req, res)=>{
+
+//get all chats of an user
+
+router.get('/v1/chats/:idUser', (req, res)=>{
+    try {
+        const { idUser } = req.params // it could be req.params.idSender or req.params.idReceiver 
+
+        const query = `SELECT users.id, users.name, users.email, users.socketid FROM users as logEdUser INNER JOIN userchat ON logEdUser.id = userchat.userId
+        INNER JOIN users ON users.id = userchat.otherUser
+        WHERE logEdUser.id = ${idUser}`
+        
+        mysql.query(query, (err, result)=>{
+            if(err) throw err
+            return res.json(result)
+        })
+    } catch (error) {
+        return res.json(null)
+    }
+})
+
+//get messages from a user
+router.get("/v1/chat/:idSender/:idReceiver", (req, res)=> {
     const { idSender, idReceiver } = req.params // it could be req.params.idSender or req.params.idReceiver 
 
     const query = `SELECT chat.id_u_sender as senderIdDb, chat.id_u_receiver as receiverIdDb,
-    users.name as who,chat.message as sms, chat.checked 
+        users.name as who,chat.message as sms, chat.checked 
         FROM chat INNER JOIN users ON chat.id_u_sender = users.id
         WHERE (id_u_sender = ${idSender} AND id_u_receiver = ${idReceiver})
         or (id_u_sender = ${idReceiver} AND id_u_receiver = ${idSender})`
